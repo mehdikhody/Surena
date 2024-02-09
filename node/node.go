d5bfef05929191d7ae8a0d9a7a4b513bce223c95
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-09 18:34:40 +0330

    Testing xray api

--- node/node.go
+++ node/node.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"surena/node/xray"
+)
 
+func main() {
+	stats, err := xray.Init().API.StatsService.QueryStats(false)
+	if err != nil {
+		return
+	}
+
+	for _, stat := range stats.GetStat() {
+		fmt.Println(stat)
+	}
 }
