ca09753fb0c15244e260d02a3f5a6bdeb6faa696
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-09 20:53:23 +0330

    Add Node struct and start functions

--- node/node.go
+++ node/node.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"surena/node/database"
+	"surena/node/scheduler"
+	"surena/node/server"
 	"surena/node/xray"
 )
 
+type Node struct {
+	db        *database.Database
+	xray      *xray.Xray
+	scheduler *scheduler.Scheduler
+	server    *server.Server
+}
+
 func main() {
-	stats, err := xray.Init().API.StatsService.QueryStats(false)
+	node := &Node{}
+	node.StartDatabase()
+	node.StartXray()
+	node.StartTasks()
+	node.StartServer()
+}
+
+func (n *Node) StartDatabase() {
+	cwd, err := os.Getwd()
 	if err != nil {
-		return
+		panic(err)
 	}
 
-	for _, stat := range stats.GetStat() {
-		fmt.Println(stat)
-	}
+	filepath := fmt.Sprintf("%s/../.bin/node-1.db", cwd)
+	n.db = database.New(filepath)
+}
+
+func (n *Node) StartXray() {
+	n.xray = xray.New()
+}
+
+func (n *Node) StartTasks() {
+	timezone := flag.String("timezone", "UTC", "Timezone")
+	flag.Parse()
+
+	fmt.Println("Timezone:", *timezone)
+
+	n.scheduler = scheduler.New(*timezone)
+	n.scheduler.Start()
+}
+
+func (n *Node) StartServer() {
+	n.server = server.New("localhost", 3000)
+	n.server.Start()
 }
