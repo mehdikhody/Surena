1c9b8732b0c87cc646a2618d04a38715576eb9ba
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-11 00:04:33 +0330

    Add scheduler initialization and start/stop functionality

--- node/scheduler/scheduler.go
+++ node/scheduler/scheduler.go
@@ -7,6 +7,8 @@ import (
 )
 
 var scheduler *Scheduler
+var schedulerInit = false
+var schedulerStarted = false
 
 type Scheduler struct {
 	cron     *cron.Cron
@@ -15,6 +17,10 @@ type Scheduler struct {
 }
 
 func New(timezone string) *Scheduler {
+	if schedulerInit {
+		return scheduler
+	}
+
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
 		panic(err)
@@ -27,23 +33,38 @@ func New(timezone string) *Scheduler {
 		),
 	}
 
+	schedulerInit = true
 	return scheduler
 }
 
 func Get() *Scheduler {
+	if !schedulerInit {
+		panic("Scheduler is not initialized")
+	}
+
 	return scheduler
 }
 
 func (s *Scheduler) Start() {
+	if schedulerStarted {
+		return
+	}
+
 	s.HtopTask = tasks.NewHtopTask()
 	s.cron.AddJob("@every 1s", s.HtopTask)
 
 	s.XrayTask = tasks.NewXrayTask()
 	s.cron.AddJob("@every 1s", s.XrayTask)
 
+	schedulerStarted = true
 	s.cron.Start()
 }
 
 func (s *Scheduler) Stop() {
+	if !schedulerStarted {
+		return
+	}
+
+	schedulerStarted = false
 	s.cron.Stop()
 }
