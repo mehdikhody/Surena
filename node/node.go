309ca396b547edb08275f384cdfb1cd585d8d0f0
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-11 00:50:26 +0330

    Refactor node start-up process

--- node/node.go
+++ node/node.go
@@ -1,55 +1,21 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+	"github.com/joho/godotenv"
 	"surena/node/database"
 	"surena/node/scheduler"
 	"surena/node/server"
 	"surena/node/xray"
 )
 
-type Node struct {
-	db        *database.Database
-	xray      *xray.Xray
-	scheduler *scheduler.Scheduler
-	server    *server.Server
-}
-
 func main() {
-	node := &Node{}
-	node.StartDatabase()
-	node.StartXray()
-	node.StartTasks()
-	node.StartServer()
-}
-
-func (n *Node) StartDatabase() {
-	cwd, err := os.Getwd()
+	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
 
-	filepath := fmt.Sprintf("%s/../.bin/node-1.db", cwd)
-	n.db = database.New(filepath)
-}
-
-func (n *Node) StartXray() {
-	n.xray = xray.New()
-}
-
-func (n *Node) StartTasks() {
-	timezone := flag.String("timezone", "UTC", "Timezone")
-	flag.Parse()
-
-	fmt.Println("Timezone:", *timezone)
-
-	n.scheduler = scheduler.New(*timezone)
-	n.scheduler.Start()
-}
-
-func (n *Node) StartServer() {
-	n.server = server.New("localhost", 3000)
-	n.server.Start()
+	database.Initialize()
+	xray.New()
+	scheduler.Initialize()
+	server.Initialize()
 }
