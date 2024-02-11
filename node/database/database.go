30d483de53afd2b524ea6b61d2e376604a7de798
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-12 01:35:32 +0330

    Add logging, error handling and separating concerns to database.go file.

--- node/database/database.go
+++ node/database/database.go
@@ -2,11 +2,13 @@ package database
 
 import (
 	"fmt"
+	"go.uber.org/zap"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"os"
 	"path/filepath"
 	"surena/node/database/models"
+	"surena/node/utils"
 	"sync"
 )
 
@@ -15,32 +17,55 @@ var databaseOnce sync.Once
 
 type Database struct {
 	db      *gorm.DB
+	logger  *zap.SugaredLogger
 	Traffic *models.TrafficModel
 	User    *models.UserModel
 	Client  *models.ClientModel
 }
 
+func initialize() error {
+	logger, err := utils.NewLogger("database")
+	if err != nil {
+		return err
+	}
+
+	logger.Info("Initializing database")
+
+	databasePath := GetFilePath()
+	logger.Infof("Database path: %s", databasePath)
+
+	databaseUri := fmt.Sprintf("file:%s?cache=shared", databasePath)
+	file := sqlite.Open(databaseUri)
+
+	db, err := gorm.Open(file, &gorm.Config{
+		PrepareStmt:          true,
+		FullSaveAssociations: true,
+	})
+
+	if err != nil {
+		logger.Warn("Failed to open database")
+		return err
+	}
+
+	clientModel, err := models.NewClientModel(db)
+	if err != nil {
+		logger.Warn("Failed to create client model")
+		return err
+	}
+
+	database = &Database{
+		db:     db,
+		logger: logger,
+		Client: clientModel,
+	}
+
+	return nil
+}
+
 func Get() (*Database, error) {
 	var err error
 	databaseOnce.Do(func() {
-		databasePath := GetFilePath()
-		databaseUri := fmt.Sprintf("file:%s?cache=shared", databasePath)
-		file := sqlite.Open(databaseUri)
-
-		database = &Database{}
-		database.db, err = gorm.Open(file, &gorm.Config{
-			PrepareStmt:          true,
-			FullSaveAssociations: true,
-		})
-
-		if err != nil {
-			return
-		}
-
-		database.Client, err = models.NewClientModel(database.db)
-		if err != nil {
-			return
-		}
+		err = initialize()
 	})
 
 	return database, err
