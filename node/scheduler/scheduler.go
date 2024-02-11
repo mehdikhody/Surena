e38a25a11b3ea9f90eae3e47117106d8ad175262
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-12 01:21:12 +0330

    Add logging, error handling, concurrency management, and separation of concerns to scheduler.

--- node/scheduler/scheduler.go
+++ node/scheduler/scheduler.go
@@ -3,8 +3,10 @@ package scheduler
 import (
 	"errors"
 	"github.com/robfig/cron/v3"
+	"go.uber.org/zap"
 	"os"
 	"surena/node/scheduler/tasks"
+	"surena/node/utils"
 	"sync"
 	"time"
 )
@@ -14,34 +16,53 @@ var schedulerOnce sync.Once
 var schedulerStarted = false
 
 type Scheduler struct {
+	sync.Mutex
 	cron     *cron.Cron
+	logger   *zap.SugaredLogger
 	HtopTask *tasks.HtopTask
-	XrayTask *tasks.XrayTask
 }
 
-func Get() *Scheduler {
+func initialize() error {
+	logger, err := utils.NewLogger("scheduler")
+	if err != nil {
+		return err
+	}
+
+	logger.Info("Initializing scheduler")
+
+	timezone := GetTimezone()
+	logger.Infof("Timezone: %s", timezone)
+
+	location, err := time.LoadLocation(timezone)
+	if err != nil {
+		logger.Warn("Failed to load timezone location")
+		return err
+	}
+
+	scheduler = &Scheduler{
+		logger: logger,
+		cron: cron.New(
+			cron.WithLocation(location),
+			cron.WithSeconds(),
+		),
+	}
+
+	scheduler.HtopTask, err = tasks.NewHtopTask(scheduler.cron)
+	if err != nil {
+		logger.Warn("Failed to create htop task")
+		return err
+	}
+
+	return nil
+}
+
+func Get() (*Scheduler, error) {
+	var err error
 	schedulerOnce.Do(func() {
-		timezone := GetTimezone()
-		location, err := time.LoadLocation(timezone)
-		if err != nil {
-			panic(err)
-		}
-
-		scheduler = &Scheduler{
-			cron: cron.New(
-				cron.WithLocation(location),
-				cron.WithSeconds(),
-			),
-		}
-
-		scheduler.HtopTask = tasks.NewHtopTask()
-		scheduler.cron.AddJob("@every 5s", scheduler.HtopTask)
-
-		scheduler.XrayTask = tasks.NewXrayTask()
-		scheduler.cron.AddJob("@every 5s", scheduler.XrayTask)
+		err = initialize()
 	})
 
-	return scheduler
+	return scheduler, err
 }
 
 func GetTimezone() string {
@@ -54,20 +75,30 @@ func GetTimezone() string {
 }
 
 func (s *Scheduler) Start() error {
+	s.Lock()
+	defer s.Unlock()
+
 	if schedulerStarted {
+		s.logger.Warn("Scheduler is already started")
 		return errors.New("scheduler is already started")
 	}
 
+	s.logger.Info("Starting scheduler")
 	schedulerStarted = true
 	s.cron.Start()
 	return nil
 }
 
 func (s *Scheduler) Stop() error {
+	s.Lock()
+	defer s.Unlock()
+
 	if !schedulerStarted {
+		s.logger.Warn("Scheduler is already stopped")
 		return errors.New("scheduler is already stopped")
 	}
 
+	s.logger.Info("Stopping scheduler")
 	schedulerStarted = false
 	s.cron.Stop()
 	return nil
