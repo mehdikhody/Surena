874ca28555f9618cc923849f16ea64305fb880c0
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-17 23:11:15 +0330

    Refactor Xray API initialization and add error handling

--- node/xray/api/api.go
+++ node/xray/api/api.go
@@ -2,27 +2,59 @@ package api
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"surena/node/utils"
 	"surena/node/xray/api/services"
+	"surena/node/xray/config"
 )
 
+var api *API
+
 type API struct {
-	client       *grpc.ClientConn
+	Logger       *logrus.Entry
+	Client       *grpc.ClientConn
 	StatsService *services.StatsService
 }
 
-func New(host string, port int) (*API, error) {
-	target := fmt.Sprintf("%s:%d", host, port)
-	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
-	client, err := grpc.Dial(target, credentials)
+type APIInterface interface {
+}
 
+func init() {
+	logger := utils.CreateLogger("xray").WithField("module", "api")
+	logger.Debug("initializing Xray api")
+
+	cf, err := config.Get()
 	if err != nil {
-		return nil, err
+		logger.Error("could not get Xray config")
+		return
 	}
 
-	return &API{
-		client:       client,
+	address, err := cf.GetAPIAddress()
+	if err != nil {
+		logger.Error("could not find Xray api address from config")
+		return
+	}
+
+	credentials := grpc.WithTransportCredentials(insecure.NewCredentials())
+	client, err := grpc.Dial(address, credentials)
+	if err != nil {
+		logger.Error("could not dial Xray api")
+		return
+	}
+
+	api = &API{
+		Logger:       logger,
+		Client:       client,
 		StatsService: services.NewStatsService(client),
-	}, nil
+	}
+}
+
+func Get() (APIInterface, error) {
+	if api == nil {
+		return nil, fmt.Errorf("xray api not initialized")
+	}
+
+	return api, nil
 }
