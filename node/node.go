3c193db74f818d52b6940850ba2ee15738e948b2
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-17 13:04:00 +0330

    Update node.go to import necessary packages and start the server

--- node/node.go
+++ node/node.go
@@ -1,43 +1,11 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	"surena/node/scheduler"
+	_ "surena/node/database"
+	_ "surena/node/scheduler"
 	"surena/node/server"
-	"surena/node/utils"
 )
 
-type Node struct {
-	scheduler *scheduler.Scheduler
-}
-
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
-	logger, err := utils.NewLogger("node")
-	if err != nil {
-		panic(err)
-	}
-
-	node := &Node{}
-	go node.StartScheduler()
-
-	logger.Debug("Starting node")
-	server.Initialize().Start()
-}
-
-func (n *Node) StartScheduler() {
-	var err error
-	n.scheduler, err = scheduler.Get()
-	if err != nil {
-		panic(err)
-	}
-
-	err = n.scheduler.Start()
-	if err != nil {
-		panic(err)
-	}
+	server.Get().Start()
 }
