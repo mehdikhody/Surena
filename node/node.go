32d4c429272240e26c3b944777d9a75c9f04fb80
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-17 16:18:34 +0330

    Add delay before starting the server

--- node/node.go
+++ node/node.go
@@ -4,8 +4,14 @@ import (
 	_ "surena/node/database"
 	_ "surena/node/scheduler"
 	"surena/node/server"
+	_ "surena/node/xray"
+	"time"
 )
 
 func main() {
+	// Wait for other services to initialize
+	// and then start the server.
+	time.Sleep(2 * time.Second)
+
 	server.Get().Start()
 }
