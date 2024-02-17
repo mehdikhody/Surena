f57cb55646f56512d62bb04ede5df4daca259f18
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-17 23:11:27 +0330

    Add logger, config, and API to Xray struct

--- node/xray/xray.go
+++ node/xray/xray.go
@@ -1,6 +1,11 @@
 package xray
 
 import (
+	"github.com/sirupsen/logrus"
+	"github.com/xtls/xray-core/common/errors"
+	"surena/node/utils"
+	"surena/node/xray/api"
+	"surena/node/xray/config"
 	"surena/node/xray/core"
 )
 
@@ -8,21 +13,42 @@ var xray *Xray
 
 type Xray struct {
 	XrayInterface
-	Core core.CoreInterface
+	Logger *logrus.Entry
+	Config config.ConfigInterface
+	Core   core.CoreInterface
+	API    api.APIInterface
 }
 
 type XrayInterface interface {
+	GetConfig() config.ConfigInterface
 	GetCore() core.CoreInterface
 }
 
 func init() {
-	xray = &Xray{
-		Core: core.Get(),
+	logger := utils.CreateLogger("xray")
+	logger.Debug("initializing Xray")
+
+	cf, err := config.Get()
+	if err != nil {
+		logger.Error("could not load Xray config")
+		return
 	}
+
+	xray = &Xray{Config: cf}
+	xray.Core, _ = core.Get()
+	xray.API, _ = api.Get()
 }
 
-func Get() XrayInterface {
-	return xray
+func Get() (XrayInterface, error) {
+	if xray == nil {
+		return nil, errors.New("Xray not initialized")
+	}
+
+	return xray, nil
+}
+
+func (x *Xray) GetConfig() config.ConfigInterface {
+	return x.Config
 }
 
 func (x *Xray) GetCore() core.CoreInterface {
