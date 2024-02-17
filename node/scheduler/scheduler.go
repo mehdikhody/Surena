323c6aef2a9ac05a75e55d72e4ab820ddfcd0213
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-17 11:48:08 +0330

    Refactor scheduler package

--- node/scheduler/scheduler.go
+++ node/scheduler/scheduler.go
@@ -2,9 +2,9 @@ package scheduler
 
 import (
 	"errors"
-	"fmt"
 	"github.com/robfig/cron/v3"
-	"go.uber.org/zap"
+	"github.com/sirupsen/logrus"
+	"surena/node/env"
 	"surena/node/scheduler/tasks"
 	"surena/node/utils"
 	"sync"
@@ -14,23 +14,25 @@ import (
 var scheduler *Scheduler
 
 type Scheduler struct {
+	SchedulerInterface
 	sync.Mutex
-	logger    *zap.SugaredLogger
-	isRunning bool
-	cron      *cron.Cron
-	HtopTask  *tasks.HtopTask
+	Logger            *logrus.Entry
+	Started           bool
+	Cron              *cron.Cron
+	SystemWatcherTask tasks.SystemWatcherTaskInterface
+}
+
+type SchedulerInterface interface {
+	IsRunning() bool
+	Start()
+	Stop()
 }
 
 func init() {
-	logger, err := utils.NewLogger("scheduler")
-	if err != nil {
-		fmt.Println("failed to create logger for scheduler")
-		return
-	}
-
+	logger := utils.CreateLogger("scheduler")
 	logger.Info("initializing scheduler")
 
-	timezone := utils.GetTimezone()
+	timezone := env.GetTimezone()
 	logger.Infof("timezone: %s", timezone)
 
 	location, err := time.LoadLocation(timezone)
@@ -39,25 +41,22 @@ func init() {
 		return
 	}
 
-	scheduler = &Scheduler{
-		logger:    logger,
-		isRunning: false,
-		cron: cron.New(
-			cron.WithLocation(location),
-			cron.WithSeconds(),
-		),
-	}
+	cronjob := cron.New(
+		cron.WithLocation(location),
+		cron.WithSeconds(),
+	)
 
-	scheduler.HtopTask, err = tasks.NewHtopTask(scheduler.cron)
-	if err != nil {
-		logger.Warn("failed to create htop task")
-		return
+	scheduler = &Scheduler{
+		Logger:            logger,
+		Started:           false,
+		Cron:              cronjob,
+		SystemWatcherTask: tasks.NewSystemWatcherTask(cronjob),
 	}
 
 	scheduler.Start()
 }
 
-func Get() (*Scheduler, error) {
+func Get() (SchedulerInterface, error) {
 	if scheduler == nil {
 		return nil, errors.New("scheduler is not initialized")
 	}
@@ -66,33 +65,33 @@ func Get() (*Scheduler, error) {
 }
 
 func (s *Scheduler) IsRunning() bool {
-	return s.isRunning
+	return s.Started
 }
 
 func (s *Scheduler) Start() {
 	s.Lock()
 	defer s.Unlock()
 
-	if s.IsRunning() {
-		s.logger.Warn("scheduler is already running")
+	if s.Started {
+		s.Logger.Warn("scheduler is already running")
 		return
 	}
 
-	s.cron.Start()
-	s.isRunning = true
-	s.logger.Info("scheduler started")
+	s.Cron.Start()
+	s.Started = true
+	s.Logger.Info("scheduler started")
 }
 
 func (s *Scheduler) Stop() {
 	s.Lock()
 	defer s.Unlock()
 
-	if !s.IsRunning() {
-		s.logger.Warn("Scheduler is already stopped")
+	if !s.Started {
+		s.Logger.Warn("Scheduler is already stopped")
 		return
 	}
 
-	s.cron.Stop()
-	s.isRunning = false
-	s.logger.Info("scheduler stopped")
+	s.Cron.Stop()
+	s.Started = false
+	s.Logger.Info("scheduler stopped")
 }
