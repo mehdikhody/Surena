819d944a8d6bdd08a2bf2930c7ee2f4f5f5e7ae0
Mehdikhody <khodayari.khoram@gmail.com>
2024-02-29 23:33:05 +0330

    Add xray and server initialization

--- node/node.go
+++ node/node.go
@@ -4,22 +4,42 @@ import (
 	"surena/node/database"
 	_ "surena/node/env"
 	"surena/node/scheduler"
+	"surena/node/server"
 	"surena/node/utils"
+	"surena/node/xray"
 )
 
 func main() {
 	logger := utils.CreateLogger("main")
 
-	_, err := database.Initialize()
+	db, err := database.Initialize()
 	if err != nil {
 		logger.Panic("Failed to initialize database")
 	}
+
+	defer db.Close()
+
+	xr, err := xray.Initialize()
+	if err != nil {
+		logger.Panic("Failed to initialize xray")
+	}
+
+	defer xr.GetCore().Stop()
+	xr.GetCore().Start()
 
 	sch, err := scheduler.Initialize()
 	if err != nil {
 		logger.Panic("Failed to initialize scheduler")
 	}
 
-	sch.Start()
 	defer sch.Stop()
+	sch.Start()
+
+	ser, err := server.Initialize()
+	if err != nil {
+		logger.Panic("Failed to initialize server")
+	}
+
+	defer ser.Stop()
+	ser.Start()
 }
